2f0cdbb1d59d908684dc29e06c799269513c90cc
dmiser23 <doe23@icloud.com>
2016-01-25 00:14:06 -0500

    Add Team and Game entities

---
+++ OfficePool.Domain/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficePool.Domain
+{
+        public class Game
+        {
+            public int GameID { get; set; }
+            public int Favorite { get; set; }
+            public decimal Spread { get; set; }
+            public int Underdog { get; set; }
+            public DateTime GameDate { get; set; }
+            public bool IsHomeTeamFavorite { get; set; }
+            public int TotalPoints { get; set; }
+            public bool IsTieBreaker { get; set; }
+        }
+}
--- OfficePool.Domain/Team.cs
+++ OfficePool.Domain/Team.cs
@@ -9,5 +9,6 @@ namespace OfficePool.Domain
     public class Team
     {
         public int TeamID { get; set; }
+        public string Name { get; set; }
     }
 }
