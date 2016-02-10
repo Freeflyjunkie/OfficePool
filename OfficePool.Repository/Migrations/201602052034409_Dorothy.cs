e5227ca27f05ea2115575fd9595c6742aaa80b6d
dmiser23 <doe23@icloud.com>
2016-02-10 17:56:50 -0500

    Dorothy's Data Migration

---
+++ OfficePool.Repository/Migrations/201602052034409_Dorothy.cs
@@ -0,0 +1,16 @@
+namespace OfficePool.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Dorothy : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
